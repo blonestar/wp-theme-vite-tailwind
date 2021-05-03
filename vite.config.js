219a62ba697e8ebf736981ff75c0339c903f4bf0
Bojan <bokicans@users.noreply.github.com>
2021-05-03 22:18:15 +0200

    server https config update, localhost certificates added

--- vite.config.js
+++ vite.config.js
@@ -12,6 +12,8 @@
 //import vue from '@vitejs/plugin-vue'
 import liveReload from 'vite-plugin-live-reload'
 const { resolve } = require('path')
+const fs = require('fs')
+
 
 // https://vitejs.dev/config
 export default {
@@ -55,18 +57,35 @@ export default {
   },
 
   server: {
+
     // required to load scripts from custom host
     cors: true,
 
     // we need a strict port to match on PHP side
-    // change freely, but update on PHP to match the same port
+    // change freely, but update in your functions.php to match the same port
     strictPort: true,
-
-    // port
     port: 3000,
 
-    // serve over http or https
-    https: true
+    // serve over http
+    https: false,
+
+    // serve over httpS
+    // to generate localhost certificate follow the link:
+    // https://github.com/FiloSottile/mkcert - Windows, MacOS and Linux supported - Browsers Chrome, Chromium and Firefox (FF MacOS and Linux only)
+    // installation example on Windows 10:
+    // > choco install mkcert (this will install mkcert)
+    // > mkcert -install (global one time install)
+    // > mkcert localhost (in project folder and files localhost-key.pem & localhost.pem will be created)
+    // uncomment below to enable https
+    //https: {
+    //  key: fs.readFileSync('localhost-key.pem'),
+    //  cert: fs.readFileSync('localhost.pem'),
+    //},
+
+    hmr: {
+      host: 'localhost',
+    },
+    
   },
 
   // required for in-browser template compilation
@@ -77,3 +96,4 @@ export default {
     }
   }
 }
+

775f0d04b7f9443c895cab2f930b7e6ea9cb9e85
Bojan <bokicans@users.noreply.github.com>
2021-05-03 22:16:44 +0200

    404 tpl added

---
+++ 404.php
@@ -0,0 +1,12 @@
+<?php get_header() ?>
+
+    <div class="w-full 2xl:container mx-auto mt-0 2xl:mt-5 p-4 border-0 2xl:border transition-all">
+
+        <h1 class="text-3xl font-semibold">404</h1>
+        <div class="mt-4">
+            <h2>Not found</h2>
+        </div>
+
+    </div>
+
+<?php get_footer() ?>

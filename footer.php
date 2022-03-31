bd83d93f07af89ae27eeea89ae188de7c61ec27a
Bojan <bokicans@users.noreply.github.com>
2022-03-31 21:40:25 +0200

    new layout created header, main & footer tags added

--- footer.php
+++ footer.php
@@ -1,3 +1,11 @@
+    </main>
+
+    <footer class="flex-0 bg-slate-100 px-4 py-2">
+        <div class="container mx-auto text-center">
+            <p class="text-xs">Currently in <strong><?php echo (IS_VITE_DEVELOPMENT) ? "development" : "production" ?></strong> mode.</p>
+        </div>
+    </footer>
+
 <?php wp_footer() ?>
 </body>
 </html>
--- header.php
+++ header.php
@@ -6,6 +6,19 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <?php wp_head() ?>
 </head>
-<body <?php body_class('h-screen border') ?>>
+<body <?php body_class('flex flex-col h-screen') ?>>
 <?php wp_body_open(); ?>
+
+    <header class="flex-0 bg-slate-100 shadow-md px-4 test-class border">
+        <div class="max-w-screen-lg mx-auto flex justify-between items-center min-h-[40px]">
+            <div class="">
+                <a href="<?php echo home_url() ?>">Logo</a>
+            </div>
+            <div>
+                <?php echo wp_nav_menu() ?>
+            </div>
+        </div>
+    </header>
+
+    <main class="flex-grow px-4 py-4">
 

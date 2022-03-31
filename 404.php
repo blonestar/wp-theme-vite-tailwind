1bc3bc6c9221e2ad980059702dc766e493040e75
Bojan <bokicans@users.noreply.github.com>
2022-03-31 21:36:34 +0200

    layout and font size fix

--- 404.php
+++ 404.php
@@ -1,8 +1,8 @@
 <?php get_header() ?>
 
-    <div class="w-full 2xl:container mx-auto mt-0 2xl:mt-5 p-4 border-0 2xl:border transition-all">
+    <div class="grid place-content-center h-full text-center">
 
-        <h1 class="text-3xl font-semibold">404</h1>
+        <h1 class="text-5xl font-semibold">404</h1>
         <div class="mt-4">
             <h2>Not found</h2>
         </div>

3c449b89bb52d948bd50fc0b080ed5d65b138124
Bojan <bokicans@users.noreply.github.com>
2022-04-10 06:28:04 +0200

    vite script logic moved to inc.vite.php

--- functions.php
+++ functions.php
@@ -3,80 +3,11 @@
 // funtions.php is empty so you can easily track what code is needed in order to Vite + Tailwind JIT run well
 
 
+// Main switch to get fontend assets from a Vite dev server OR from production built folder
+// it is recommeded to move it into wp-config.php
+define('IS_VITE_DEVELOPMENT', false);
 
-/*
- *
- * VITE & Tailwind JIT development
- * Inspired by https://github.com/andrefelipe/vite-php-setup
- * v0.2.1
- *
- */
 
-// Main switch to get fontend assets from a Vite dev server or from production built folder
-// it is recommeded to move the define into wp-config.php
-define('IS_VITE_DEVELOPMENT', true);
+include "inc/inc.vite.php";
 
-// dist subfolder - defined in vite.config.json
-define('DIST_DEF', 'dist');
-
-// defining some base urls and paths
-define('DIST_URI', get_template_directory_uri() . '/' . DIST_DEF);
-define('DIST_PATH', get_template_directory() . '/' . DIST_DEF);
-
-// js enqueue settings
-define('JS_DEPENDENCY', array()); // array('jquery') as example
-define('JS_LOAD_IN_FOOTER', true); // load scripts in footer?
-
-// server address, port and entry point can be custmized from vite.config.json
-define('VITE_SERVER', 'http://localhost:3000');
-define('VITE_ENTRY_POINT', '/main.js');
-
-// enqueue hook
-function enqueue_scripts_and_styles() {
-    
-    
-    if (defined('IS_VITE_DEVELOPMENT') && IS_VITE_DEVELOPMENT === true) {
-
-        // 'npm run dev' running
-        // ----------
-
-        function vite_head_module_hook() {
-            echo '<script type="module" crossorigin src="' . VITE_SERVER . VITE_ENTRY_POINT . '"></script>';
-        }
-        add_action('wp_head', 'vite_head_module_hook');        
-
-    } else {
-
-        // production version, 'npm run build' must be executed in order to generate assets
-        // ----------
-
-        // read manifest.json to figure out what to enqueue
-        $manifest = json_decode( file_get_contents( DIST_PATH . '/manifest.json'), true );
-        
-        // is ok
-        if (is_array($manifest)) {
-            
-            // get first key, by default is 'main.js' but it can change
-            $manifest_key = array_keys($manifest);
-            if (isset($manifest_key[0])) {
-                
-                // enqueue CSS files
-                foreach(@$manifest[$manifest_key[0]]['css'] as $css_file) {
-                    wp_enqueue_style( 'main', DIST_URI . '/' . $css_file );
-                }
-                
-                // enqueue main JS file
-                $js_file = @$manifest[$manifest_key[0]]['file'];
-                if ( ! empty($js_file)) {
-                    wp_enqueue_script( 'main', DIST_URI . '/' . $js_file, JS_DEPENDENCY, '', JS_LOAD_IN_FOOTER );
-                }
-                
-            }
-
-        }
-
-    }
-
-}
-add_action( 'wp_enqueue_scripts', 'enqueue_scripts_and_styles' );
 
---
+++ inc/inc.vite.php
@@ -0,0 +1,67 @@
+<?php
+/*
+ * VITE & Tailwind JIT development
+ * Inspired by https://github.com/andrefelipe/vite-php-setup
+ *
+ */
+
+// dist subfolder - defined in vite.config.json
+define('DIST_DEF', 'dist');
+
+// defining some base urls and paths
+define('DIST_URI', get_template_directory_uri() . '/' . DIST_DEF);
+define('DIST_PATH', get_template_directory() . '/' . DIST_DEF);
+
+// js enqueue settings
+define('JS_DEPENDENCY', array()); // array('jquery') as example
+define('JS_LOAD_IN_FOOTER', true); // load scripts in footer?
+
+// deafult server address, port and entry point can be customized in vite.config.json
+define('VITE_SERVER', 'http://localhost:3000');
+define('VITE_ENTRY_POINT', '/main.js');
+
+// enqueue hook
+add_action( 'wp_enqueue_scripts', function() {
+    
+    if (defined('IS_VITE_DEVELOPMENT') && IS_VITE_DEVELOPMENT === true) {
+
+        // insert hmr into head for live reload
+        function vite_head_module_hook() {
+            echo '<script type="module" crossorigin src="' . VITE_SERVER . VITE_ENTRY_POINT . '"></script>';
+        }
+        add_action('wp_head', 'vite_head_module_hook');        
+
+    } else {
+
+        // production version, 'npm run build' must be executed in order to generate assets
+        // ----------
+
+        // read manifest.json to figure out what to enqueue
+        $manifest = json_decode( file_get_contents( DIST_PATH . '/manifest.json'), true );
+        
+        // is ok
+        if (is_array($manifest)) {
+            
+            // get first key, by default is 'main.js' but it can change
+            $manifest_key = array_keys($manifest);
+            if (isset($manifest_key[0])) {
+                
+                // enqueue CSS files
+                foreach(@$manifest[$manifest_key[0]]['css'] as $css_file) {
+                    wp_enqueue_style( 'main', DIST_URI . '/' . $css_file );
+                }
+                
+                // enqueue main JS file
+                $js_file = @$manifest[$manifest_key[0]]['file'];
+                if ( ! empty($js_file)) {
+                    wp_enqueue_script( 'main', DIST_URI . '/' . $js_file, JS_DEPENDENCY, '', JS_LOAD_IN_FOOTER );
+                }
+                
+            }
+
+        }
+
+    }
+
+
+});

154367d2476d1e85896b908aa3f73f585d5d453f
Bojan <bokica@gmail.com>
2022-09-20 21:43:09 +0200

    class reorder

--- index.php
+++ index.php
@@ -1,31 +1,31 @@
 <?php get_header() ?>
 
-<div class="max-w-screen-lg mx-auto h-full flex place-content-center items-center">
+<div class="place-content-center flex items-center h-full max-w-screen-lg mx-auto">
     <div class="text-center">
         <h1 class="text-4xl font-bold text-black">Hello World!</h1>
         <h2 class="text-xl font-bold text-black">Wordpress Theme rapid development using Vite & Tailwindcss</h2>
         <div class="text-md mt-4">
-            <a class="inline-block rounded-lg my-1 py-1 px-4 text-white bg-purple-700 hover:opacity-90" href="https://vitejs.dev/" target="_blank">ViteJS</a>
-            <a class="inline-block rounded-lg my-1 py-1 px-4 text-white bg-green-600 hover:opacity-90" href="https://tailwindcss.com/" target="_blank">TailwindCSS</a>
+            <a class="hover:opacity-90 inline-block px-4 py-1 my-1 text-white bg-purple-700 rounded-lg" href="https://vitejs.dev/" target="_blank">ViteJS</a>
+            <a class="hover:opacity-90 inline-block px-4 py-1 my-1 text-white bg-green-600 rounded-lg" href="https://tailwindcss.com/" target="_blank">TailwindCSS</a>
         </div>
 
         <p class="mt-4 text-sm">To test browser sync in development mode ensure the following:</p>
-        <code class="mt-1 px-4 py-1 bg-slate-800 text-gray-200 text-xs text-left inline-block">
+        <code class="bg-slate-800 inline-block px-4 py-1 mt-1 text-xs text-left text-gray-200">
             <p class="my-2">
-                <span class="text-gray-500 block">// change IS_VITE_DEVELOPMENT to TRUE in functions.php</span>
+                <span class="block text-gray-500">// define IS_VITE_DEVELOPMENT in functions or wp-config.php</span>
                 define('IS_VITE_DEVELOPMENT', TRUE);
             </p>
             <p class="my-2">
-                <span class="text-gray-500 block">// run first time in your theme folder (node.js required)</span>
+                <span class="block text-gray-500">// run first time in your <strong>theme folder</strong> (node.js required)</span>
                 npm install
             </p>
             <p class="my-2">
-                <span class="text-gray-500 block">// start development & refresh your browser</span>
+                <span class="block text-gray-500">// start development & refresh your browser</span>
                 npm run dev
             </p>
         </code>
 
-        <p class="mt-1 text-xs">More instructions <a class="hover:underline" href="https://github.com/blonestar/wp-theme-vite-tailwind" target="_blank">here</a>.</p>
+        <p class="mt-1 text-xs"><a class="hover:underline" href="https://github.com/blonestar/wp-theme-vite-tailwind" target="_blank">More instructions here</a>.</p>
 
     </div>
 </div>

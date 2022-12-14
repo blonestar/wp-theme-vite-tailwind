a79d63afae73b380839b06c3e54de640bb4626a0
Bojan <bokica@gmail.com>
2022-12-14 11:07:29 +0100

    forgotten man var

--- functions.php
+++ functions.php
@@ -9,7 +9,7 @@ if ( ! defined( 'ABSPATH' ) )
 
 // Main switch to get fontend assets from a Vite dev server OR from production built folder
 // it is recommeded to move it into wp-config.php
-define('IS_VITE_DEVELOPMENT', false);
+define('IS_VITE_DEVELOPMENT', true);
 
 
 include "inc/inc.vite.php";

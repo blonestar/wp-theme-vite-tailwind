ac5d068b553fb28fbb9c59a71aa86a22d5b40f7c
Floris <florisdouma@gmail.com>
2023-06-08 19:50:35 +0200

    Fixed typos in functions.php

--- functions.php
+++ functions.php
@@ -4,11 +4,11 @@
 if ( ! defined( 'ABSPATH' ) )
     exit;  
 
-// funtions.php is empty so you can easily track what code is needed in order to Vite + Tailwind JIT run well
+// functions.php is empty so you can easily track what code is needed in order to Vite + Tailwind JIT run well
 
 
-// Main switch to get fontend assets from a Vite dev server OR from production built folder
-// it is recommeded to move it into wp-config.php
+// Main switch to get frontend assets from a Vite dev server OR from production built folder
+// it is recommended to move it into wp-config.php
 define('IS_VITE_DEVELOPMENT', true);
 
 
